d5a123820815e2ed38ec276a1751a56648d9ece9
Test <aovolkov1@gmail.com>
2024-11-22 02:17:40 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 13686;      // Стоимость билета
+        int b = a / 20;      // Формула расчета бонусных миль
+
+        System.out.println(b + " бонусные мили");      // Вывод результатов
+
+
+    }
+}
